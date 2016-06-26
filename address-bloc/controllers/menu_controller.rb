4da0849557723ee7a11bc42a8230a15c262db817
Robert Aguirre <raguirre.cofc@gmail.com>
2016-06-26 20:22:19 +0000

    Checkpoint 23:Completed

--- address-bloc/controllers/menu_controller.rb
+++ address-bloc/controllers/menu_controller.rb
@@ -1,5 +1,3 @@
-
- # #1
  require_relative '../models/address_book'
  
  class MenuController
@@ -10,7 +8,6 @@
    end
  
    def main_menu
-     # #2
      puts "Main Menu - #{address_book.entries.count} entries"
      puts "1 - View all entries"
      puts "2 - Create an entry"
@@ -19,10 +16,9 @@
      puts "5 - Exit"
      print "Enter your selection: "
  
-     # #3
      selection = gets.to_i
           
-    case selection
+     case selection
        when 1
          system "clear"
          view_all_entries
@@ -41,9 +37,7 @@
          main_menu
        when 5
          puts "Good-bye!"
-         # #8
          exit(0)
-       # #9
        else
          system "clear"
          puts "Sorry, that is not a valid input"
@@ -51,13 +45,11 @@
      end
    end
  
-   # #10
    def view_all_entries
-     # #14
      address_book.entries.each do |entry|
        system "clear"
        puts entry.to_s
-     # #15
+
        entry_submenu(entry)
      end
  
@@ -68,7 +60,7 @@
    def create_entry
      system "clear"
      puts "New AddressBloc Entry"
-     # #12
+
      print "Name: "
      name = gets.chomp
      print "Phone number: "
@@ -76,7 +68,6 @@
      print "Email: "
      email = gets.chomp
  
-     # #13
      address_book.add_entry(name, phone, email)
  
      system "clear"
@@ -84,36 +75,113 @@
    end
  
    def search_entries
-   end
- 
-   def read_csv
+     print "Search by name: "
+     name = gets.chomp
+    
+     match = address_book.binary_search(name)
+     system "clear"
+    
+     if match
+       puts match.to_s
+       search_submenu(match)
+     else
+       puts "No match found for #{name}"
+     end    
    end
    
-   def entry_submenu(entry)
-     # #16
-     puts "n - next entry"
-     puts "d - delete entry"
+   def search_submenu(entry)
+     puts "\nd - delete entry"
      puts "e - edit this entry"
      puts "m - return to main menu"
- 
-     # #17
+
      selection = gets.chomp
  
      case selection
-     # #18
-       when "n"
-     # #19
        when "d"
+         system "clear"
+         delete_entry(entry)
+         main_menu
        when "e"
-     # #20
+         edit_entry(entry)
+         system "clear"
+         main_menu
        when "m"
          system "clear"
          main_menu
        else
          system "clear"
          puts "#{selection} is not a valid input"
+         puts entry.to_s
+         search_submenu(entry)
+     end
+   end
+ 
+   def read_csv
+     print "Enter CSV file to import: "
+     file_name = gets.chomp
+ 
+     if file_name.empty?
+       system "clear"
+       puts "No CSV file read"
+       main_menu
+     end
+ 
+     begin
+       entry_count = address_book.import_from_csv(file_name).count
+       system "clear"
+       puts "#{entry_count} new entries added from #{file_name}"
+     rescue
+       puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+       read_csv
+     end
+   end
+   
+   def entry_submenu(entry)
+     puts "n - next entry"
+     puts "d - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+ 
+     selection = gets.chomp
+ 
+     case selection
+       when "n"
+       when "d"
+         delete_entry(entry)
+       when "e"
+         edit_entry(entry)
+         entry_submenu(entry)
+       when "m"
+         system "clear"
+         main_menu
+         
+       else
+         system "clear"
+         puts "#{selection} is not a valid input"
          entry_submenu(entry)
      end
    end
+   
+   def delete_entry(entry)
+     address_book.entries.delete(entry)
+     puts "#{entry.name} has been deleted"
+   end
+   
+   def edit_entry(entry)
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+     
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
 
+     puts "Updated entry:"
+     puts entry
+   end
+    
  end
