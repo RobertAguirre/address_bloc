22b35c10faf7e2fd51fbe5fdd9b9d1d02b24a605
Robert Aguirre <raguirre.cofc@gmail.com>
2016-06-26 19:03:15 +0000

    Checkpoint 22 Assignment:Completed

--- address-bloc/models/address_book.rb
+++ address-bloc/models/address_book.rb
@@ -53,4 +53,15 @@
       return nil
    end
    
+   # Search AddressBook for a specific entry by name
+   def iterative_search(name)
+     @entries.each do |entry|
+       if entry.name == name
+         return entry
+       end
+     end
+     
+     return nil
+   end
+   
  end
